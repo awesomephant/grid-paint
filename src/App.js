f2a0a31f65ba5f99e51baf5bd5c492f1a66210ae
Max Kohler <hi@maxkoehler.com>
2019-11-30 19:46:51 +0000

    Add control panel

--- src/App.js
+++ src/App.js
@@ -3,9 +3,39 @@ import Grid from './Grid.js';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      settings: {
+        columns: 10,
+        rows: 5
+      }
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    let v = e.target.value;
+    let setting = e.target.getAttribute('id');
+    this.setState(function (prevState) {
+      prevState.settings[setting] = v
+      return prevState
+    })
+  }
   render() {
     return (
-      <Grid columns='8' rows='16'></Grid>
+      <div className='app'>
+        <div className='controls'>
+          <div className='controls--field'>
+            <label htmlFor='rows'>Rows</label>
+            <input type='number' value={this.state.settings.rows} onChange={this.handleChange} id='rows'></input>
+          </div>
+          <div className='controls--field'>
+            <label htmlFor='columns'>Columns</label>
+            <input type='number' value={this.state.settings.columns} onChange={this.handleChange} id='columns'></input>
+          </div>
+        </div>
+        <Grid columns={this.state.settings.columns} rows={this.state.settings.rows}></Grid>
+      </div>
     );
   }
 }
